63a92aadc2a4644b1ffdd9b9ce8a3bc40a66cdeb
CassF <cassandrafrimpong@gmail.com>
2017-02-07 15:33:30 +0000

    pull develop

--- models/spartan.js
+++ models/spartan.js
@@ -3,78 +3,78 @@ const moment = require("moment");
 
 //create spartan schema
 let SpartanSchema = mongoose.Schema({
-    firstName: {
-        type: String,
-        required: true
-    },
+   firstName: {
+       type: String,
+       required: true
+   },
 
-    lastName: {
-        type: String,
-        required: true
-    },
+   lastName: {
+       type: String,
+       required: true
+   },
 
-    dob: {
-        type: Date
-    },
-    course: {
-        type: String
-    },
+   dob: {
+       type: Date
+   },
+   course: {
+       type: String
+   },
 
-    course_Id: {
-        type: String
-    },
+   course_Id: {
+       type: String
+   },
 
-    address: {
-        type: String
-    },
+   address: {
+       type: String
+   },
 
-    email: {
-        type: String,
-        required: true,
-        unique: true
+   email: {
+       type: String,
+       required: true,
+       unique: true
 
-    },
+   },
 
-    phoneNumber: {
-        type: String,
-        required: true
-    },
+   phoneNumber: {
+       type: String,
+       required: true
+   },
 
-    emergencyContactName: {
-        type: String,
-    },
+   emergencyContactName: {
+       type: String,
+   },
 
-    emergencyContactNo: {
-        type: String,
-    },
+   emergencyContactNo: {
+       type: String,
+   },
 
-    instructor: {
-        type: Boolean,
-        default: false
-    },
+   instructor: {
+       type: Boolean,
+       default: false
+   },
 
-    slackHandle: {
-        type: String
-    },
+   slackHandle: {
+       type: String
+   },
 
-    applicant: {
-        type: Boolean,
-        default: false
-    },
+   applicant: {
+       type: Boolean,
+       default: false
+   },
 
-    inTraining: {
-        type: Boolean,
-        default: false
-    },
-    clientSite: {
-        type: Boolean,
-        default: false
-  },
+   inTraining: {
+       type: Boolean,
+       default: false
+   },
+   clientSite: {
+       type: Boolean,
+       default: false
+ },
 
-    onBench: {
-        type: Boolean,
-        default: false
-    }
+   onBench: {
+       type: Boolean,
+       default: false
+   }
 
 
 
