fda2c24f919ff9addd3634ccaab2722320952983
Cassandra Frimpong-Yiadom <Cassandra Frimpong-Yiadom>
2017-02-23 19:44:18 +0000

    refactored code: created lib folder to chain functions

--- controllers/paymentController.js
+++ controllers/paymentController.js
@@ -1,51 +1,15 @@
-const Payment = require("../models/payment");
-const Youth = require("../models/youth");
+const Payment = require("../lib/payment");
 
-class paymentController {
-
-    static createPayment(req) {
-        return new Promise(
-            (resolve, reject) => {
-                Payment.create(req.body, function (err, payment) {
-                    console.log(payment);
-                    if (err) {
-                        reject(err);
-                    } else {
-                        return new Promise(
-                            (reject, resolve) => {
-                                console.log(res);
-                                Youth.findByIdAndUpdate(payment.youth_Id, { $inc: { balance: req.body.amount } }, function (err, youth) {
-                                    if (err) {
-                                        reject(err);
-                                    } else {
-                                        resolve();
-                                        console.log("findbyidandupdate worked");
-                                    }
-                                });
-                            }
-                        )
-                    }
-                });
-            }
-        )
+class PaymentController {
+    static newPayment(req, res) {
+        Payment.createPayment(req)
+            .then((result) => {
+                Payment.addPaymentToBalance(req, res)
+            })
+            .catch(err => {
+                res.status(400).send(err.message);
+            });
     }
-
-    static addPaymentToBalance(req, result) {
-        return new Promise(
-            (reject, resolve) => {
-                //console.log(resolveArray);
-                Youth.findByIdAndUpdate(result.id, { $inc: { balance: req.body.amount } }, function (err, res) {
-                    if (err) {
-                        reject(err);
-                    } else {
-                        resolve();
-                        console.log("findbyidandupdate worked");
-                    }
-                });
-            }
-        )
-    }
-
 }
 
-module.exports = paymentController;
+module.exports = PaymentController;
