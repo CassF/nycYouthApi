cb5a66de312cf3bc815b069d71994eb17f2f0529
Cassandra Frimpong-Yiadom <Cassandra Frimpong-Yiadom>
2017-02-23 23:27:25 +0000

    removed promises

--- controllers/youthController.js
+++ controllers/youthController.js
@@ -3,17 +3,15 @@ const Youth = require("../models/youth");
 class youthController {
     //promise to handle the creation of a new user
     static createYouth(req) {
-        return new Promise(
-            (resolve, reject) => {
-                Youth.create(req.body, function (err, youth) {
-                    if (err) {
-                        reject(err);
-                    } else {
-                        resolve(youth);
-                    }
-                });
+        Youth.create(req.body, function (err, youth) {
+            if (err) {
+                res.status(400).send(err.message);
+            } else {
+                res.status(200).send({
+                    registeredYouth: youth
+                })
             }
-        )
+        });
     }
 
     static showAllYouth(req, res) {
