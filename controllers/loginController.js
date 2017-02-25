1e230b54a273df35796a223ea97f4b1202820c4f
Cassandra Frimpong-Yiadom <Cassandra Frimpong-Yiadom>
2017-02-25 13:03:49 +0000

    fixed the login issue

--- controllers/loginController.js
+++ controllers/loginController.js
@@ -1,10 +1,17 @@
 const hash = require('password-hash');
 const password = process.env.PW;
+const username = process.env.USERNAME;
 
 class LoginController {
     static verifyPassword(req, res){
-        let checkedPassword = hash.verify(password, req.body.password);
-        checkedPassword ? res.status(200).send(checkedPassword) : res.status(400).send('error');
+        if(req.body.username == username){
+            let hashedPassword = hash.generate(req.body.password);
+            let checkedPassword = hash.verify(password, hashedPassword);
+            checkedPassword ? res.status(200).send(checkedPassword) : res.status(400).send('error');
+        }else{
+            console.log("sec");
+            res.status(400).send('error');
+        }
     }
 }
 
